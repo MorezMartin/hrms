ac2c3cb204a5b51620453102f981a7233fee43f1
Morez Martin <morez.martin@gmail.com>
2023-06-10 18:04:33 +0200

    try to enhance Sales Order HR

--- hrms/hr/report/sales_order_hr/sales_order_hr.py
+++ hrms/hr/report/sales_order_hr/sales_order_hr.py
@@ -134,7 +134,6 @@ def get_sales_orders(filters=None):
 			['name', 'delivery_date', 'customer', 'shipping_address_name']
 			)
 	if filters.get('items'):
-		filters.items = frappe.parse_json(filters.get('items'))
 		frappe.msgprint(filters.items)
 	for so in sos:
 		items = frappe.db.get_all('Sales Order Item', {'parent': so['name'], 'item_code': ['in', filters.items]}, ['item_code', 'qty', 'uom', 'description'])
@@ -244,8 +243,6 @@ def get_summary(filters=None):
 	lsos = len(sos)
 	srqs, sass, tls, items = 0, 0, 0, 0
 	human_needs = 0
-	if filters.get('items'):
-		filters.items = frappe.parse_json(filters.get('items'))
 	for so in sos:
 		items = frappe.db.get_all('Sales Order Item', {'parent': so['name'], 'item_code': ['in', filters.items]}, ['qty'])
 		for item in items:
