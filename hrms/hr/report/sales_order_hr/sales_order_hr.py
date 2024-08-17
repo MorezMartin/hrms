05bb6d28655e7bcda9c8bec600b52cd8a595d52e
marty <morez.martin@gmail.com>
2024-08-17 16:47:33 +0200

    Fix syntax error -> try to enhance sales order links section section in sohr

--- hrms/hr/report/sales_order_hr/sales_order_hr.py
+++ hrms/hr/report/sales_order_hr/sales_order_hr.py
@@ -145,7 +145,7 @@ def get_avaibilities(filters=None):
 				ts = {}
 			if e == None:
 				e = {}
-			line = {**sav, **srq, **sa, **ts, 'indent': 2, e}
+			line = {**sav, **srq, **sa, **ts, 'indent': 2, **e}
 		emps.append(line)
 	return emps
 
